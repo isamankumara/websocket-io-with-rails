b5d940d418221e0ac88b51f759029c6b58ed7e96
isaman kumara <me@isamankumara.com>
2018-04-03 11:28:14 +0530

    add init commit for xamarin.ios with websocket

---
+++ ViewController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using WebSocket4Net;
+
+using UIKit;
+using SuperSocket.ClientEngine;
+
+namespace SocketApp
+{
+    public partial class ViewController : UIViewController
+    {
+        WebSocket websocket = new WebSocket("ws://192.168.8.104:3000/cable");
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            websocket.Opened += new EventHandler(websocket_Opened);
+            websocket.Error += new EventHandler<ErrorEventArgs>(websocket_Error);
+            websocket.Closed += new EventHandler(websocket_Closed);
+            websocket.MessageReceived += new EventHandler<MessageReceivedEventArgs>(websocket_MessageReceived);
+            websocket.Open();
+
+            base.ViewDidLoad();
+        }
+
+        private void websocket_Error(object sender, ErrorEventArgs e)
+        {
+        }
+
+        private void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        { 
+        }
+
+        private void websocket_Closed(object sender, EventArgs e)
+        {
+        }
+
+        private void websocket_Opened(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ ViewController.designer.cs
@@ -0,0 +1,20 @@
+﻿//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+
+namespace SocketApp
+{
+    [Register("ViewController")]
+    partial class ViewController
+    {
+        [Action("tapMessage:")]
+        [GeneratedCode("iOS Designer", "1.0")]
+        partial void tapMessage(UIKit.UIButton sender);
+        void ReleaseDesignerOutlets()
+        {
+        }
+    }
+}
